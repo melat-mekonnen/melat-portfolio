151e8cb08b742cb3b7d8010250c67796927315d9
melat-mekonnen <127474013+melat-mekonnen@users.noreply.github.com>
2025-05-06 09:31:02 -0700

    adding html some css and js

---
+++ src/script.js
@@ -0,0 +1,53 @@
+	// Sticky Navbar
+    let header = document.querySelector('header');
+    let menu = document.querySelector('#menu-icon');
+    let navbar = document.querySelector('.navbar');
+     
+     
+    window.addEventListener('scroll', () => {
+        header.classList.toggle('shadow', window.scrollY > 0);
+    });
+     
+    menu.onclick = () => {
+        navbar.classList.toggle('active');
+    }
+    window.onscroll = () => {
+        navbar.classList.remove('active');
+    }
+     
+    // Dark Mode
+    let darkmode = document.querySelector('#darkmode');
+     
+    darkmode.onclick = () => {
+        if(darkmode.classList.contains('bx-moon')){
+            darkmode.classList.replace('bx-moon','bx-sun');
+            document.body.classList.add('active');
+        }else{
+            darkmode.classList.replace('bx-sun','bx-moon');
+            document.body.classList.remove('active');
+        }
+    }
+    function openTab(evt, tabName) {
+        // Get all elements with class="tab-panel" and hide them
+        var i, tabcontent, tabbuttons;
+        tabcontent = document.getElementsByClassName("tab-panel");
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+        }
+
+        // Get all elements with class="tab-btn" and remove the class "active"
+        tabbuttons = document.getElementsByClassName("tab-btn");
+        for (i = 0; i < tabbuttons.length; i++) {
+            tabbuttons[i].className = tabbuttons[i].className.replace(" active", "");
+        }
+
+        // Show the current tab, and add an "active" class to the button that opened the tab
+        document.getElementById(tabName).style.display = "block";
+        evt.currentTarget.className += " active";
+    }
+
+    // Show the default tab on page load
+    document.addEventListener("DOMContentLoaded", function() {
+        document.getElementById("skills").style.display = "block"; // Default to "Skills" tab
+        document.querySelector('.tab-btn').classList.add('active'); // Make the first tab button active
+    });
